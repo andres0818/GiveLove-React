447669526decc5c9def546d6f9d6a5d6d9d7efc3
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2023-03-03 16:07:03 -0500

    feat: install firebase funcional

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -6,7 +6,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import Loader from "../../components/loader/Loader";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -39,6 +40,7 @@ const Register = () => {
 
   return (
     <>
+    <ToastContainer/>
       {isLoading && <Loader />}
       <section className={`container ${styles.auth}`}>
         <Card>
