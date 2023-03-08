729850610af1ba9a1fd37247198db882f0048866
Giovani Mesa <giovaniitj@gmail.com>
2023-03-07 21:41:46 -0500

    feat: admin

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+
 //Componentes
 import { Header, Footer } from './components/index';
 //Pages
 import { Home, Contact, Login, Register, Reset } from './pages/index';
+
+import Create from "./components/firebasee/Create";
+import Edit from "./components/firebasee/Edit";
+import Show from './components/firebasee/Show';
 
 function App() {
   return (
@@ -15,6 +20,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/show" element={<Show />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/edit/:id" element={<Edit />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
