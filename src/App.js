efc683ba892338e5823e9b684a8c771b13190734
YennyMTB <112446763+YennyMTB@users.noreply.github.com>
2023-03-12 09:59:00 -0500

    fusion cambios Giovani

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+//import { ToastContainer } from "react-toastify";
+//import "react-toastify/dist/ReactToastify.css";
 // Pages
 import { Home, Contact, Login, Register, Reset, Admin } from "./pages";
 // Components
@@ -20,9 +20,9 @@ import ReviewProducts from "./components/reviewProducts/ReviewProducts";
 import Form from "./components/form/Form.jsx";
 
 //Componentes
-import { Header, Footer } from './components/index';
+//import { Header, Footer } from './components/index';
 //Pages
-import { Home, Contact, Login, Register, Reset } from './pages/index';
+//import { Home, Contact, Login, Register, Reset } from './pages/index';
 
 import Create from "./components/firebasee/Create";
 import Edit from "./components/firebasee/Edit";
