e95d69bf055df07b2af068e7e7f3e9788750987c
andres0818 <c.andres.perez.h@gmail.com>
2023-03-22 00:52:38 -0500

    :D

--- src/components/label/Labels.jsx
+++ src/components/label/Labels.jsx
@@ -13,7 +13,7 @@ const Labels = () => {
           type="checkbox"
           checked={checked}
           onChange={() =>
-            updateLabel({ label: lbl, checked: !checked })
+            updateLabel({ label: lbl, checked: !checked }) 
           }
           className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded focus:ring-0 cursor-pointer`}
         />
